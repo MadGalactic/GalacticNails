5f9086cd2b66851086595356a6fab8be0b8220e6
MVDLNE <madelinelora95@icloud.com>
2025-06-21 23:22:56 -0400

    added spring security dependency

--- src/main/java/com/example/GalacticNails/controllers/HomeController.java
+++ src/main/java/com/example/GalacticNails/controllers/HomeController.java
@@ -2,7 +2,20 @@ package com.example.GalacticNails.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+/*
+ created by Madeline Lora
+ */
 @Controller
 public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String home() {
+        return "Hello, Spring!";
+    }
+
 }
