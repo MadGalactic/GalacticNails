3f2ea84227187bbedd60c23a98956ee615970690
MVDLNE <madelinelora95@icloud.com>
2025-06-21 22:58:17 -0400

    adding controller for home page

---
+++ src/main/java/com/example/GalacticNails/GalacticNailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GalacticNails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticNailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticNailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GalacticNails/GalacticNailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GalacticNails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticNailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
