b67952b5f4c47a5468a7b105eca3aa9ad4871e00
MVDLNE <madelinelora95@icloud.com>
2025-06-25 11:03:08 -0400

    modifying controller

--- src/main/java/com/example/GalacticNails/controllers/HomeController.java
+++ src/main/java/com/example/GalacticNails/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.example.GalacticNails.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -12,10 +14,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
     @ResponseBody
-    public String home() {
-        return "Hello, Spring!";
+    public String goodbye(){
+        return "Goodbye, spring";
+    }
+
+    // handles request of the form
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    // handles requests of the form /hello/GalacticNails
+    // this handler method is looking for requests to /hello/name where the name can be anything
+    // will take the value of that piece of data and place it in the method parameter name
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello " + name + "<3";
     }
 
 }
